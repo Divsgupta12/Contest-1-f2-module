bf8b679e35f07d26e6fdf1324c84a8401bdbdb28
Divsgupta12 <divsgupta2000@gmail.com>
2023-01-07 21:43:07 +0530

    first commit

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  let newArr = arr.map(myfunction);
+  function myfunction(item) {
+    if(item.profession == "developer")
+    return item;
+  }
+  console.log(newArr);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(myfunction);
+  function myfunction(item) {
+    if(item.profession == "developer")
+    console.log(item);
+  }
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr);
 }
 
 function removeAdmin() {
@@ -24,4 +37,11 @@ function removeAdmin() {
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, state : "uttar pradesh", city : "Lucknow" , pincode : "226012" },
+    { id: 6, brand : "oppo", device : "phone" , modelno : "28888383" },
+    { id: 7, item : "pen", color : "black" , tip : "0.5 mm" },
+  ];
+  let array = arr.concat(arr2);
+  console.log(array);
 }
